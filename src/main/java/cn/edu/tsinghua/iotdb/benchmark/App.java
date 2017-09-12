d58c2ba06ff855fdd21f869aab27f3e2c333aa27
Dark Templar <xuyi556677@163.com>
2017-09-12 14:26:16 +0800

    init

---
+++ src/main/java/cn/edu/tsinghua/iotdb/benchmark/App.java
@@ -0,0 +1,13 @@
+package cn.edu.tsinghua.iotdb.benchmark;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
