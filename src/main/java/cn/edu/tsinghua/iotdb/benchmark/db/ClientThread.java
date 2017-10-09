63128922213124d16fc11b97de3a9740967883c0
XuYi <xuyithuss@126.com>
2017-10-09 19:30:38 +0800

    add error for init database

--- src/main/java/cn/edu/tsinghua/iotdb/benchmark/db/ClientThread.java
+++ src/main/java/cn/edu/tsinghua/iotdb/benchmark/db/ClientThread.java
@@ -2,6 +2,9 @@ package cn.edu.tsinghua.iotdb.benchmark.db;
 
 import java.sql.SQLException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import cn.edu.tsinghua.iotdb.benchmark.conf.Config;
 import cn.edu.tsinghua.iotdb.benchmark.conf.ConfigDescriptor;
 
@@ -9,6 +12,7 @@ public class ClientThread implements Runnable{
 	private IDatebase database;
 	private int index;
 	private Config config;
+	private static final Logger LOOGER = LoggerFactory.getLogger(ClientThread.class);
 	
 	public ClientThread(IDatebase datebase, int index) {
 		this.database = datebase;
@@ -23,15 +27,14 @@ public class ClientThread implements Runnable{
 		try {
 			database.init();
 		} catch (SQLException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
+			LOOGER.error("{} Fail to init database becasue {}",Thread.currentThread().getName(), e1.getMessage());
+			return;
 		}
 		while(i < config.LOOP){
 			try {
 				database.insertOneBatch(config.DEVICE_CODES.get(index), i);
 			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+				LOOGER.error("{} Fail to insert one batch into database becasue {}",Thread.currentThread().getName(), e.getMessage());
 			}
 			i++;
 		}
