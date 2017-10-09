503db23ebeec7a383d2ba2fc2eb81110ed9a4010
XuYi <xuyithuss@126.com>
2017-10-09 19:34:07 +0800

    add error for init database

--- src/main/java/cn/edu/tsinghua/iotdb/benchmark/App.java
+++ src/main/java/cn/edu/tsinghua/iotdb/benchmark/App.java
@@ -4,6 +4,9 @@ import java.sql.SQLException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import cn.edu.tsinghua.iotdb.benchmark.conf.Config;
 import cn.edu.tsinghua.iotdb.benchmark.conf.ConfigDescriptor;
 import cn.edu.tsinghua.iotdb.benchmark.db.ClientThread;
@@ -11,6 +14,7 @@ import cn.edu.tsinghua.iotdb.benchmark.db.IDatebase;
 import cn.edu.tsinghua.iotdb.benchmark.db.IoTDB;
 
 public class App {
+	private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		CommandCli cli = new CommandCli();
@@ -18,10 +22,17 @@ public class App {
 			return;
 		}
 		Config config = ConfigDescriptor.getInstance().getConfig();
-		IDatebase datebase = new IoTDB();
-		datebase.init();
-		datebase.createSchema();
-		datebase.close();
+		IDatebase datebase;
+		try {
+			datebase = new IoTDB();
+			datebase.init();
+			datebase.createSchema();
+			datebase.close();
+		} catch (SQLException e) {
+			LOGGER.error("Fail to init database becasue {}",e.getMessage());
+			return;
+		}
+		
 		ExecutorService executorService = Executors.newFixedThreadPool(config.DEVICE_NUMBER);
 		for(int i = 0; i < config.DEVICE_NUMBER;i++){
 			executorService.submit(new ClientThread(new IoTDB(), i));
