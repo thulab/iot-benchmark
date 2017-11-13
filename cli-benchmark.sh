899ab3c2d7d5678042619f54c0b5508a2bfae01d
liuruiyiyang <245422695@qq.com>
2017-11-13 11:19:19 +0800

    add pre-remove log-stop-flag

--- cli-benchmark.sh
+++ cli-benchmark.sh
@@ -2,12 +2,12 @@
 
 mvn clean package -Dmaven.test.skip=true
 
-ssh hadoop@192.168.130.16 'sh /home/hadoop/liurui/github/iotdb-benchmark/ser-benchmark.s > /dev/null 2>&1 &'
+ssh hadoop@192.168.130.16 "rm /home/hadoop/liurui/log_stop_flag;sh /home/hadoop/liurui/github/iotdb-benchmark/ser-benchmark.sh > /dev/null 2>&1 &"
 echo '------Client Test Begin Time------'
 date
 cd bin
 sh startup.sh -cf ../conf/config.properties
 wait
-ssh hadoop@192.168.130.16 'touch /home/hadoop/liurui/log_stop_flag'
+ssh hadoop@192.168.130.16 "touch /home/hadoop/liurui/log_stop_flag"
 echo '------Client Test Complete Time------'
 date
