95f22ea0561f3776b28f5e91a9c788aaea0c5b7b
liuruiyiyang <245422695@qq.com>
2017-11-13 13:43:21 +0800

    initial database in server by cli-benchmark.sh

--- cli-benchmark.sh
+++ cli-benchmark.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+if [ -z "${BENCHMARK_HOME}" ]; then
+
+  export BENCHMARK_HOME="$(cd "`dirname "$0"`"/.; pwd)"
+
+fi
+
+#change to client mode
+sed -i 's/SERVER_MODE *= *true/SERVER_MODE=false/g' $BENCHMARK_HOME/conf/config.properties
+
 mvn clean package -Dmaven.test.skip=true
 
 ssh hadoop@192.168.130.16 "rm /home/hadoop/liurui/log_stop_flag;sh /home/hadoop/liurui/github/iotdb-benchmark/ser-benchmark.sh > /dev/null 2>&1 &"
@@ -11,3 +20,7 @@ wait
 ssh hadoop@192.168.130.16 "touch /home/hadoop/liurui/log_stop_flag"
 echo '------Client Test Complete Time------'
 date
+
+#initial database in server
+ssh hadoop@192.168.130.16 "sh /home/hadoop/xuyi/iotdb/iotdb/bin/stop-server.sh;rm -rf data;sh /home/hadoop/xuyi/iotdb/iotdb/bin/start-server.sh"
+
