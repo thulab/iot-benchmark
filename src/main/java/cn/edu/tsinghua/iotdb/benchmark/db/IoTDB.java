1cb421521e5f3f42f4209f67256b3c7f38d5f47f
liuruiyiyang <245422695@qq.com>
2017-11-21 17:17:05 +0800

    fix bug

--- src/main/java/cn/edu/tsinghua/iotdb/benchmark/db/IoTDB.java
+++ src/main/java/cn/edu/tsinghua/iotdb/benchmark/db/IoTDB.java
@@ -188,6 +188,8 @@ public class IoTDB implements IDatebase {
 	public void insertOneBatch(String device, int loopIndex,
 			ThreadLocal<Long> totalTime, ThreadLocal<Long> errorCount) {
 		Statement statement;
+		int[] result ;
+		int errorNum = 0;
 		try {
 			statement = connection.createStatement();
 			for (int i = 0; i < config.CACHE_NUM; i++) {
@@ -195,33 +197,30 @@ public class IoTDB implements IDatebase {
 				statement.addBatch(sql);
 			}
 			long startTime = System.currentTimeMillis();
-			statement.executeBatch();
+			result = statement.executeBatch();
 			statement.clearBatch();
 			statement.close();
 			long endTime = System.currentTimeMillis();
-			LOGGER.info(
-					"{} execute {} loop, it costs {}s, totalTime {}s, throughput {} points/s",
-					Thread.currentThread().getName(),
-					loopIndex,
-					(endTime - startTime) / 1000.0,
-					(totalTime.get() + (endTime - startTime)) / 1000.0,
-					(config.CACHE_NUM * config.SENSOR_NUMBER / (double) (endTime - startTime)) * 1000);
-			totalTime.set(totalTime.get() + (endTime - startTime));
-		} catch (BatchUpdateException e) {
-			// TODO Auto-generated catch block
-			int[] ret = e.getUpdateCounts();
-			int diff = config.CACHE_NUM - ret.length;
-			if (diff == 0) {// same--遇到error继续执行；不同则说明只返回了正确的个数；
-				for (int i : ret) {
-					if (i == Statement.EXECUTE_FAILED) {
-						diff++;
-					}
+
+			for(int i=0;i<result.length;i++){
+				if(result[i]==-1){
+					errorNum++;
 				}
 			}
-			errorCount.set(errorCount.get() + diff);
-			LOGGER.error("Batch insert failed, the failed num is {}! Error：{}",
-					diff, e.getMessage());
-			e.printStackTrace();
+
+			if(errorNum>0) {
+				LOGGER.info("Batch insert failed, the failed number is {}! ", errorNum);
+			}else{
+				LOGGER.info(
+						"{} execute {} loop, it costs {}s, totalTime {}s, throughput {} points/s",
+						Thread.currentThread().getName(),
+						loopIndex,
+						(endTime - startTime) / 1000.0,
+						(totalTime.get() + (endTime - startTime)) / 1000.0,
+						(config.CACHE_NUM * config.SENSOR_NUMBER / (double) (endTime - startTime)) * 1000);
+				totalTime.set(totalTime.get() + (endTime - startTime));
+				errorCount.set(errorCount.get() + errorNum);
+			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
@@ -234,6 +233,8 @@ public class IoTDB implements IDatebase {
 		// TODO Auto-generated method stub
 
 		Statement statement;
+		int[] result ;
+		int errorNum = 0;
 		try {
 			statement = connection.createStatement();
 			for (String sql : cons) {
@@ -241,33 +242,29 @@ public class IoTDB implements IDatebase {
 			}
 
 			long startTime = System.currentTimeMillis();
-			statement.executeBatch();
+			result = statement.executeBatch();
 			statement.clearBatch();
 			statement.close();
 			long endTime = System.currentTimeMillis();
 
-			LOGGER.debug(
-					"{} execute {} loop, it costs {}s, totalTime {}s, throughput {} items/s",
-					Thread.currentThread().getName(), batchIndex,
-					(endTime - startTime) / 1000.0,
-					((totalTime.get() + (endTime - startTime)) / 1000.0),
-					(cons.size() / (double) (endTime - startTime)) * 1000);
-			totalTime.set(totalTime.get() + (endTime - startTime));
-		} catch (BatchUpdateException e) {
-			// TODO Auto-generated catch block
-			int[] ret = e.getUpdateCounts();
-			int diff = cons.size() - ret.length;
-			if (diff == 0) {// same--遇到error继续执行；不同则说明只返回了正确的个数；
-				for (int i : ret) {
-					if (i == Statement.EXECUTE_FAILED) {
-						diff++;
-					}
+			for(int i=0;i<result.length;i++){
+				if(result[i]==-1){
+					errorNum++;
 				}
 			}
-			errorCount.set(errorCount.get() + diff);
-			LOGGER.error("Batch insert failed, the failed num is {}! Error：{}",
-					diff, e.getMessage());
-			e.printStackTrace();
+
+			if(errorNum>0) {
+				LOGGER.info("Batch insert failed, the failed number is {}! ", errorNum);
+			}else{
+				LOGGER.debug(
+						"{} execute {} loop, it costs {}s, totalTime {}s, throughput {} items/s",
+						Thread.currentThread().getName(), batchIndex,
+						(endTime - startTime) / 1000.0,
+						((totalTime.get() + (endTime - startTime)) / 1000.0),
+						(cons.size() / (double) (endTime - startTime)) * 1000);
+				totalTime.set(totalTime.get() + (endTime - startTime));
+				errorCount.set(errorCount.get() + errorNum);
+			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
@@ -342,8 +339,12 @@ public class IoTDB implements IDatebase {
 					Constants.ROOT_SERIES_NAME + "." + path + "."
 							+ sensor));
 			if((count%1000)==0){
+				long startTime = System.currentTimeMillis();
 				statement.executeBatch();
 				statement.clearBatch();
+				long endTime = System.currentTimeMillis();
+				LOGGER.info("batch create timeseries execute speed ,{},timeseries/s",
+						1000000.0f/(endTime-startTime));
 				if(count>=timeseriesTotal){
 					statement.close();
 				}
