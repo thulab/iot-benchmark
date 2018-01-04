92499b7f449ebc723adc2a0079c9a22e5c8901c3
yi xu <yixu@yis-MacBook-Pro.local>
2018-01-04 21:43:12 +0800

    add read config file

--- src/main/java/cn/edu/tsinghua/iotdb/benchmark/concurrency/App.java
+++ src/main/java/cn/edu/tsinghua/iotdb/benchmark/concurrency/App.java
@@ -3,6 +3,7 @@ package cn.edu.tsinghua.iotdb.benchmark.concurrency;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import cn.edu.tsinghua.iotdb.benchmark.CommandCli;
 import cn.edu.tsinghua.iotdb.benchmark.conf.Config;
 import cn.edu.tsinghua.iotdb.benchmark.conf.ConfigDescriptor;
 import cn.edu.tsinghua.iotdb.jdbc.TsfileJDBCConfig;
@@ -10,6 +11,11 @@ import cn.edu.tsinghua.iotdb.jdbc.TsfileJDBCConfig;
 public class App {
 
 	public static void main(String[] args) throws ClassNotFoundException{
+		CommandCli cli = new CommandCli();
+		if (!cli.init(args)) {
+			return;
+		}
+		
 		Class.forName(TsfileJDBCConfig.JDBC_DRIVER_NAME);
 		ExecutorService cachedThreadPool = Executors.newCachedThreadPool();
 		Config config = ConfigDescriptor.getInstance().getConfig();
