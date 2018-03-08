187c420fcee3c2e2906071655435552279f2ebb4
liuruiyiyang <245422695@qq.com>
2018-03-08 15:50:54 +0800

    denbug

--- src/main/java/cn/edu/tsinghua/iotdb/benchmark/sersyslog/MemUsage.java
+++ src/main/java/cn/edu/tsinghua/iotdb/benchmark/sersyslog/MemUsage.java
@@ -142,13 +142,13 @@ public class MemUsage {
                 String line = null;
                 while ((line = in.readLine()) != null) {
                     if(line.startsWith("map")) {
-                        String[] temp = line.split("\\s");
+                        String[] temp = line.split(" ");
                         for(String s:temp){
                             log.info(s);
                         }
-                        log.info("temp[3]:" + temp[3]);
-                        String[] tmp = temp[3].split("K");
-                        String size = tmp[0];
+                        log.info("temp[3]:" + temp[6]);
+                        String[] tmp = temp[6].split("K");
+                        String size = tmp[6];
                         processMemUsage = Long.parseLong(size) / KB2GB;
                     }
                 }
