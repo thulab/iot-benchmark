198a459c8a824051bec5e4e13f46469e4f7831d2
liuruiyiyang <245422695@qq.com>
2018-06-07 23:53:14 +0800

    add constant

--- src/main/java/cn/edu/tsinghua/iotdb/benchmark/conf/Constants.java
+++ src/main/java/cn/edu/tsinghua/iotdb/benchmark/conf/Constants.java
@@ -25,7 +25,7 @@ public class Constants {
 	public static final String MYSQL_DRIVENAME = "com.mysql.jdbc.Driver";
 	
 	public static final String[] QUERY_CHOICE_NAME = {" ","Precise","Fuzzy","Aggregation",
-		"Range","Criteria","Nearest Point","Group By","SLimit","Limit Criteria"};
+		"Range","Criteria","Nearest Point","Group By","SLimit","Limit Criteria", "Aggregation Without Filter", "Aggregation With Value Filter"};
 
 	public static final String SAMPLE_DATA_FILE_NAME = "sampleData.txt";
 	
