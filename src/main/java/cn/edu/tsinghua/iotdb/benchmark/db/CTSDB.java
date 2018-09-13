577d1ebd20107fc16bfec68eccd930f1aa2c98a6
liuruiyiyang <245422695@qq.com>
2018-09-13 13:59:10 +0800

    modify cli-benchmark.sh

--- src/main/java/cn/edu/tsinghua/iotdb/benchmark/db/CTSDB.java
+++ src/main/java/cn/edu/tsinghua/iotdb/benchmark/db/CTSDB.java
@@ -10,16 +10,13 @@ import cn.edu.tsinghua.iotdb.benchmark.model.TSDBDataModel;
 import cn.edu.tsinghua.iotdb.benchmark.mysql.MySqlLog;
 import cn.edu.tsinghua.iotdb.benchmark.utils.HttpRequest;
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONArray;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.net.Authenticator;
-import java.net.HttpURLConnection;
 import java.net.PasswordAuthentication;
 import java.sql.SQLException;
-import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
