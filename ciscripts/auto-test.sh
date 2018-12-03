d2f4a5bb5178cec71a3fb623b2a8c8a9cfa24625
liuruiyiyang <245422695@qq.com>
2018-12-03 21:34:52 +0800

    add universal ci script

---
+++ ciscripts/auto-test.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+ROUTINE=$1
+IOTDB_CONF=$2
+DATABASE=$3
+MYSQL_HOST=$4
+
+git pull
+echo "begin testing the performance..."
+cp ./archive/$ROUTINE  ./routine
+./rep-benchmark.sh $IOTDB_CONF
+mysql -A $DATABASE -h $MYSQL_HOST -uroot -pIse_Nel_2017  -e "SELECT queryResult.projectID,queryResult.avg,queryResult.midAvg,queryResult.min,queryResult.max,queryResult.p1,queryResult.p5,queryResult.p50,queryResult.p90,queryResult.p95,queryResult.totalTimes,queryResult.totalPoints,queryResult.queryNumber from queryResult;" | sed 's/\t/","/g;s/^/"/;s/$/"/;s/\n//g' > queryResult.csv
+#tail -n 3 ./logs/log_info.log | cut -d '-' -f 4 > result.txt
