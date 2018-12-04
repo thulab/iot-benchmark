0f04f8bbb80dc3e331d5a2125aa02c22186a3134
liuruiyiyang <245422695@qq.com>
2018-12-04 10:27:55 +0800

    add version retrieve to benchmark.sh

--- benchmark.sh
+++ benchmark.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 
+if [ -z "${BENCHMARK_HOME}" ]; then
+  export BENCHMARK_HOME="$(cd "`dirname "$0"`"/.; pwd)"
+fi
+
+HOST_NAME=liurui
+IP=$(grep "HOST" $BENCHMARK_HOME/conf/config.properties)
+FLAG_AND_DATA_PATH=$(grep "LOG_STOP_FLAG_PATH" $BENCHMARK_HOME/conf/config.properties)
+SERVER_HOST=$HOST_NAME@${IP#*=}
+LOG_STOP_FLAG_PATH=${FLAG_AND_DATA_PATH#*=}
+
 git pull
+COMMIT_ID=$(ssh $SERVER_HOST "cd $LOG_STOP_FLAG_PATH/iotdb;git tag -l | tail -n 1")" commit_id:"$(ssh $SERVER_HOST "cd $LOG_STOP_FLAG_PATH/iotdb;git rev-parse HEAD")
+sed -i "s/^VERSION.*$/VERSION=${COMMIT_ID}/g" $BENCHMARK_HOME/conf/config.properties
 rm -rf ./lib
 mvn clean package -Dmaven.test.skip=true
 cd bin
