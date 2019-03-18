3a99b70e5039e94a42f8874556be9b668b725745
liukun <451564319@qq.com>
2019-03-18 15:10:17 +0800

    add startup

--- bin/startup.sh
+++ bin/startup.sh
@@ -25,7 +25,52 @@ else
     JAVA=java
 fi
 
+pid=$$
+
+
+echo "================="
+echo "the job pid is ${pid}"
+echo "================="
+
+mkdir ${pid}
+
+cd ${pid}
+
+pidstat -p ${pid} -u  5 > cpu.log &
+pidstat -p ${pid} -r  5 > mem.log &
+pidstat -p ${pid} -w 5 > context.log &
+iostat -x 5 > iotstat.log &
+
+cd ..
+
 
 exec "$JAVA" -Duser.timezone=GMT+8 -Dlogback.configurationFile=${BENCHMARK_HOME}/conf/logback.xml  -cp "$CLASSPATH" "$MAIN_CLASS" "$@"
+
+#====== monitor kill code
+
+killpid=$(ps -ux | grep "liurui" | grep "iostat" -m 1 | awk '{print $2}')
+
+echo "========iostat pid======="
+echo ${killpid}
+
+kill -9 ${killpid}
+
+killpid=$(ps -ux | grep "liurui" | grep "pidstat" -m 1 | awk '{print $2}')
+
+kill -9 ${killpid}
+
+killpid=$(ps -ux | grep "liurui" | grep "pidstat" -m 1 | awk '{print $2}')
+
+kill -9 ${killpid}
+
+killpid=$(ps -ux | grep "liurui" | grep "pidstat" -m 1 | awk '{print $2}')
+
+kill -9 ${killpid}
+
+echo "================="
+echo "the job pid is ${pid}"
+echo "================="
+
+#===== monitor kill code
 
 exit $?
