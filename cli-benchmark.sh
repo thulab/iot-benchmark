156017a90148e578f1a469928230f2cecef2ae93
liuruiyiyang <245422695@qq.com>
2019-04-03 10:55:54 +0800

    test

--- cli-benchmark.sh
+++ cli-benchmark.sh
@@ -97,7 +97,7 @@ else
     if [ "${DB#*=}" = "IoTDB" -a "${BENCHMARK_WORK_MODE#*=}" = "insertTestWithDefaultPath" ]; then
         echo "initial database in server..."
         if [ $IS_TEST_BASELINE = "true" ]; then
-            ssh $SERVER_HOST "cd $LOG_STOP_FLAG_PATH;rm -rf ./baseline_iotdb;mkdir ./baseline_iotdb;cd ./baseline_iotdb;git clone https://github.com/apache/incubator-iotdb.git;cd ./incubator-iotdb;git checkout v0.7.0;mvn clean package -Dmaven.test.skip=true"
+            ssh $SERVER_HOST "cd $LOG_STOP_FLAG_PATH;rm -rf ./baseline_iotdb;rm -rf ./log_stop_flag;mkdir ./baseline_iotdb;cd ./baseline_iotdb;git clone https://github.com/apache/incubator-iotdb.git;cd ./incubator-iotdb;git checkout v0.7.0;mvn clean package -Dmaven.test.skip=true"
             ssh $SERVER_HOST "sh $LOG_STOP_FLAG_PATH/baseline_iotdb/incubator-iotdb/iotdb/bin/stop-server.sh;sleep 5"
             #start server system information recording
             ssh $SERVER_HOST "sh $LOG_STOP_FLAG_PATH/iotdb-benchmark/ser-benchmark.sh > /dev/null 2>&1 &"
@@ -107,7 +107,7 @@ else
             scp $BENCHMARK_HOME/$IOTDB_CONF/iotdb-env.sh $SERVER_HOST:$LOG_STOP_FLAG_PATH/baseline_iotdb/incubator-iotdb/iotdb/conf
             ssh $SERVER_HOST "sh $LOG_STOP_FLAG_PATH/baseline_iotdb/incubator-iotdb/iotdb/bin/start-server.sh > /dev/null 2>&1 &"
         else
-            ssh $SERVER_HOST "cd $LOG_STOP_FLAG_PATH;rm -rf ./incubator-iotdb;git clone https://github.com/apache/incubator-iotdb.git;cd ./incubator-iotdb;mvn clean package -Dmaven.test.skip=true"
+            ssh $SERVER_HOST "cd $LOG_STOP_FLAG_PATH;rm -rf ./incubator-iotdb;rm -rf ./log_stop_flag;git clone https://github.com/apache/incubator-iotdb.git;cd ./incubator-iotdb;mvn clean package -Dmaven.test.skip=true"
             ssh $SERVER_HOST "sh $LOG_STOP_FLAG_PATH/incubator-iotdb/iotdb/iotdb/bin/stop-server.sh;sleep 5"
             #start server system information recording
             ssh $SERVER_HOST "sh $LOG_STOP_FLAG_PATH/iotdb-benchmark/ser-benchmark.sh > /dev/null 2>&1 &"
