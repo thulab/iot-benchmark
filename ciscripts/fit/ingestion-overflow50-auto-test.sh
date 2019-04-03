7717b9a8b1a967056695e02be4341077a82a3359
liuruiyiyang <245422695@qq.com>
2019-04-03 10:37:28 +0800

    test

--- ciscripts/fit/ingestion-overflow50-auto-test.sh
+++ ciscripts/fit/ingestion-overflow50-auto-test.sh
@@ -4,5 +4,5 @@ git pull
 echo "begin testing the performance..."
 cp ./archive/fit/ingestion-overflow50-auto-test  ./routine
 ./rep-benchmark.sh iotdbconf/fit false
-mysql -A auto_test -h 166.111.7.145 -uroot -pIse_Nel_2017  -e "SELECT insertResult.*, configInsertInfo.version from insertResult  JOIN configInsertInfo USING(projectID)" | sed 's/\t/","/g;s/^/"/;s/$/"/;s/\n//g' > insertResult.csv
+mysql -A auto_test -h 166.111.7.145 -P 33306 -uroot -pIse_Nel_2017  -e "SELECT insertResult.*, configInsertInfo.version from insertResult  JOIN configInsertInfo USING(projectID)" | sed 's/\t/","/g;s/^/"/;s/$/"/;s/\n//g' > insertResult.csv
 #tail -n 3 ./logs/log_info.log | cut -d '-' -f 4 > result.txt
