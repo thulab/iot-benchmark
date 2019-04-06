477b2cfe8ed6400f0801d551a41c6d448e3bfbb5
qiaojialin <646274302@qq.com>
2019-04-06 14:53:09 +0800

    add aggregation and groupby query definition

---
+++ src/main/java/cn/edu/tsinghua/iotdb/benchmark/workload/query/impl/AggFunction.java
@@ -0,0 +1,5 @@
+package cn.edu.tsinghua.iotdb.benchmark.workload.query.impl;
+
+public enum AggFunction{
+  COUNT, MAX_VALUE, MIN_VALUE, AVG
+}
--- src/main/java/cn/edu/tsinghua/iotdb/benchmark/workload/query/impl/GroupByQuery.java
+++ src/main/java/cn/edu/tsinghua/iotdb/benchmark/workload/query/impl/GroupByQuery.java
@@ -1,7 +1,41 @@
 package cn.edu.tsinghua.iotdb.benchmark.workload.query.impl;
 
 import cn.edu.tsinghua.iotdb.benchmark.workload.query.Query;
+import cn.edu.tsinghua.iotdb.benchmark.workload.schema.DeviceSchema;
+import java.util.List;
+
 
 public class GroupByQuery extends Query {
+
+  private List<DeviceSchema> deviceSchema;
+  private AggFunction aggFun;
+  // also used to be the segment start time
+  private long startTimestamp;
+  private long endTimestamp;
+  private long interval;
+
+  public GroupByQuery(List<DeviceSchema> deviceSchema, AggFunction aggFun, long startTimestamp, long endTimestamp, long interval) {
+    this.deviceSchema = deviceSchema;
+    this.aggFun = aggFun;
+    this.startTimestamp = startTimestamp;
+    this.endTimestamp = endTimestamp;
+    this.interval = interval;
+  }
+
+  public AggFunction getAggFun() {
+    return aggFun;
+  }
+
+  public long getStartTimestamp() {
+    return startTimestamp;
+  }
+
+  public long getEndTimestamp() {
+    return endTimestamp;
+  }
+
+  public List<DeviceSchema> getDeviceSchema() {
+    return deviceSchema;
+  }
 
 }
