85544843b69c8eaff02d2390355ed8032258533b
liuruiyiyang <245422695@qq.com>
2019-04-12 09:44:36 +0800

    fix review

--- src/main/java/cn/edu/tsinghua/iotdb/benchmark/tsdb/IDatabase.java
+++ src/main/java/cn/edu/tsinghua/iotdb/benchmark/tsdb/IDatabase.java
@@ -59,7 +59,7 @@ public interface IDatabase {
 
   /**
    * Query data of one or multiple sensors in a time range.
-   * e.g. select v1... from data where time > ? and time < ? and device in ?
+   * e.g. select v1... from data where time >= ? and time <= ? and device in ?
    * @param rangeQuery universal range query condition parameters
    * @return status which contains successfully executed flag, error message and so on.
    */
@@ -67,16 +67,16 @@ public interface IDatabase {
 
   /**
    * Query data of one or multiple sensors in a time range with a value filter.
-   * e.g. select v1... from data where time > ? and time < ? and v1 > ? and device in ?
-   * @param valueRangeQuery contains universal range query with value filter condition parameters
+   * e.g. select v1... from data where time >= ? and time <= ? and v1 > ? and device in ?
+   * @param valueRangeQuery contains universal range query with value filter parameters
    * @return status which contains successfully executed flag, error message and so on.
    */
   Status valueRangeQuery(ValueRangeQuery valueRangeQuery);
 
   /**
    * Query aggregated data of one or multiple sensors in a time range using aggregation function.
-   * e.g. select func(v1)... from data where device in ? and time > ? and time < ?
-   * @param aggRangeQuery contains universal aggregation query with time filter condition parameters
+   * e.g. select func(v1)... from data where device in ? and time >= ? and time <= ?
+   * @param aggRangeQuery contains universal aggregation query with time filter parameters
    * @return status which contains successfully executed flag, error message and so on.
    */
   Status aggRangeQuery(AggRangeQuery aggRangeQuery);
@@ -84,15 +84,15 @@ public interface IDatabase {
   /**
    * Query aggregated data of one or multiple sensors in the whole time range.
    * e.g. select func(v1)... from data where device in ? and value > ?
-   * @param aggValueQuery contains universal aggregation query with value filter condition parameters
+   * @param aggValueQuery contains universal aggregation query with value filter parameters
    * @return status which contains successfully executed flag, error message and so on.
    */
   Status aggValueQuery(AggValueQuery aggValueQuery);
 
   /**
    * Query aggregated data of one or multiple sensors with both time and value filters.
-   * e.g. select func(v1)... from data where device in ? and time > ? and time < ? and value > ?
-   * @param aggRangeValueQuery contains universal aggregation query with time and value filters condition parameters
+   * e.g. select func(v1)... from data where device in ? and time >= ? and time <= ? and value > ?
+   * @param aggRangeValueQuery contains universal aggregation query with time and value filters parameters
    * @return status which contains successfully executed flag, error message and so on.
    */
   Status aggRangeValueQuery(AggRangeValueQuery aggRangeValueQuery);
@@ -100,9 +100,9 @@ public interface IDatabase {
   /**
    * Query aggregated group-by-time data of one or multiple sensors within a time range.
    * e.g.
-   * SELECT max(s 0), max(s 1)
-   * FROM group 0 , group 1
-   * WHERE ( device = ’d 3’ OR device = ’d 8’)
+   * SELECT max(s_0), max(s_1)
+   * FROM group_0, group_1
+   * WHERE ( device = ’d_3’ OR device = ’d_8’)
    * AND time >= 2010-01-01 12:00:00 AND time <= 2010-01-01 12:10:00
    * GROUP BY time(60000ms)
    * @param groupByQuery contains universal group by query condition parameters
@@ -112,6 +112,7 @@ public interface IDatabase {
 
   /**
    * Query the latest(max-timestamp) data of one or multiple sensors.
+   * e.g. select time, v1... where device = ? and time = max(time)
    * @param latestPointQuery contains universal latest point query condition parameters
    * @return status which contains successfully executed flag, error message and so on.
    */
