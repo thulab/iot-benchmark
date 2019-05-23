0712f91e088b2a2cb00c37af8d3895c2f5e5d3c5
liuruiyiyang <245422695@qq.com>
2019-05-23 13:27:50 +0800

    change script

--- benchmark.sh
+++ benchmark.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+if [ -z "${BENCHMARK_HOME}" ]; then
+  export BENCHMARK_HOME="$(cd "`dirname "$0"`"; pwd)"
+fi
+
 #git pull
 rm -rf lib
 mvn clean package -Dmaven.test.skip=true
 cd bin
-sh startup.sh -cf ../conf/config.properties
+$BENCHMARK_HOME/bin/startup.sh -cf ../conf/config.properties
