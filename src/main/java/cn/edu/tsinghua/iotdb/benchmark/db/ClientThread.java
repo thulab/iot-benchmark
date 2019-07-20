cf6e8b43d0b28ae0bf5fbaaf2328023a0c4d9a2b
EJTTianyu <1653503269@qq.com>
2019-07-20 20:16:13 +0800

    add some info

--- src/main/java/cn/edu/tsinghua/iotdb/benchmark/db/ClientThread.java
+++ src/main/java/cn/edu/tsinghua/iotdb/benchmark/db/ClientThread.java
@@ -15,62 +15,65 @@ import cn.edu.tsinghua.iotdb.benchmark.conf.Config;
 import cn.edu.tsinghua.iotdb.benchmark.conf.ConfigDescriptor;
 import cn.edu.tsinghua.iotdb.benchmark.loadData.Storage;
 
-public class ClientThread implements Runnable{
-	private IDatebase database;
-	private MySqlLog mySql;
-	private int index;
-	private Config config;
-	private static final Logger LOGGER = LoggerFactory.getLogger(ClientThread.class);
-	private Storage storage;
-	private static ThreadLocal<Long> totalTime = new ThreadLocal<Long>(){
-		protected Long initialValue(){
-			return  (long) 0;
-		}
-	};
-	private static ThreadLocal<Long> errorCount = new ThreadLocal<Long>(){
-		protected Long initialValue(){
-			return  (long) 0;
-		}
-	};
-	private CountDownLatch downLatch;
-	private ArrayList<Long> totalTimes;
-	private ArrayList<Long> totalInsertErrorNums;
-	private ArrayList<Long> latencies;
-	private ArrayList<ArrayList> latenciesOfClients;
+public class ClientThread implements Runnable {
 
-	public ClientThread(IDatebase datebase, int index, CountDownLatch downLatch,
-			ArrayList<Long> totalTimes, ArrayList<Long> totalInsertErrorNums, ArrayList<ArrayList> latenciesOfClients) {
-		this.config = ConfigDescriptor.getInstance().getConfig();
-		this.database = datebase;
-		this.index = index;
-		this.downLatch = downLatch;
-		this.totalTimes = totalTimes;
-		this.totalInsertErrorNums = totalInsertErrorNums;
-		this.latencies = new ArrayList<>();
-		this.latenciesOfClients = latenciesOfClients;
-		mySql = new MySqlLog();
-		mySql.initMysql(datebase.getLabID());
-	}
+  private IDatebase database;
+  private MySqlLog mySql;
+  private int index;
+  private Config config;
+  private static final Logger LOGGER = LoggerFactory.getLogger(ClientThread.class);
+  private Storage storage;
+  private static ThreadLocal<Long> totalTime = new ThreadLocal<Long>() {
+    protected Long initialValue() {
+      return (long) 0;
+    }
+  };
+  private static ThreadLocal<Long> errorCount = new ThreadLocal<Long>() {
+    protected Long initialValue() {
+      return (long) 0;
+    }
+  };
+  private CountDownLatch downLatch;
+  private ArrayList<Long> totalTimes;
+  private ArrayList<Long> totalInsertErrorNums;
+  private ArrayList<Long> latencies;
+  private ArrayList<ArrayList> latenciesOfClients;
 
-	public ClientThread(IDatebase datebase, int index , Storage storage, CountDownLatch downLatch,
-			ArrayList<Long> totalTimes, ArrayList<Long> totalInsertErrorNums, ArrayList<ArrayList> latenciesOfClients) {
-		this.config = ConfigDescriptor.getInstance().getConfig();
-		this.database = datebase;
-		this.index = index;
-		this.storage = storage;
-		this.downLatch = downLatch;
-		this.totalTimes = totalTimes;
-		this.totalInsertErrorNums = totalInsertErrorNums;
-		this.latencies = new ArrayList<>();
-		this.latenciesOfClients = latenciesOfClients;
-		mySql = new MySqlLog();
-		mySql.initMysql(datebase.getLabID());
-	}
+  public ClientThread(IDatebase datebase, int index, CountDownLatch downLatch,
+      ArrayList<Long> totalTimes, ArrayList<Long> totalInsertErrorNums,
+      ArrayList<ArrayList> latenciesOfClients) {
+    this.config = ConfigDescriptor.getInstance().getConfig();
+    this.database = datebase;
+    this.index = index;
+    this.downLatch = downLatch;
+    this.totalTimes = totalTimes;
+    this.totalInsertErrorNums = totalInsertErrorNums;
+    this.latencies = new ArrayList<>();
+    this.latenciesOfClients = latenciesOfClients;
+    mySql = new MySqlLog();
+    mySql.initMysql(datebase.getLabID());
+  }
+
+  public ClientThread(IDatebase datebase, int index, Storage storage, CountDownLatch downLatch,
+      ArrayList<Long> totalTimes, ArrayList<Long> totalInsertErrorNums,
+      ArrayList<ArrayList> latenciesOfClients) {
+    this.config = ConfigDescriptor.getInstance().getConfig();
+    this.database = datebase;
+    this.index = index;
+    this.storage = storage;
+    this.downLatch = downLatch;
+    this.totalTimes = totalTimes;
+    this.totalInsertErrorNums = totalInsertErrorNums;
+    this.latencies = new ArrayList<>();
+    this.latenciesOfClients = latenciesOfClients;
+    mySql = new MySqlLog();
+    mySql.initMysql(datebase.getLabID());
+  }
 
 
-	@Override
-	public void run() {
-		int i = 0;
+  @Override
+  public void run() {
+    int i = 0;
 //		try {
 //			database.init();
 //		} catch (SQLException e1) {
@@ -78,130 +81,145 @@ public class ClientThread implements Runnable{
 //			return;
 //		}
 
-		if(config.READ_FROM_FILE){
-			while(true){
-				try {
-					LinkedList<String> cons = storage.consume(config.BATCH_OP_NUM);
-					//线程结束
-					if(cons.size() == 0) {
-						break;
-					}
-					database.insertOneBatch(cons , i, totalTime, errorCount, latencies);
-				} catch (SQLException e) {
-					LOGGER.error("{} Fail to insert one batch into database becasue {}",Thread.currentThread().getName(), e.getMessage());
-				}
-				i++;
-			}
-		}
-		else{
-			int clientDevicesNum = config.DEVICE_NUMBER/config.CLIENT_NUMBER;
-			LinkedList<String> deviceCodes = new LinkedList<>();
-			//may not correct in multiple device per batch mode
-			long pointsOneLoop = config.DEVICE_NUMBER / config.CLIENT_NUMBER * config.SENSOR_NUMBER * config.BATCH_SIZE;
-			double actualLoopSecond = (double) pointsOneLoop / config.CLIENT_MAX_WRT_RATE;
-			//overflow mode 2 related variables initial
-			Random random = new Random(config.QUERY_SEED);
-			ArrayList<Integer> before = new ArrayList<>();
-			int maxIndex = (int) (config.BATCH_SIZE * config.LOOP * config.OVERFLOW_RATIO);
-			int currMaxIndexOfDist = config.START_TIMESTAMP_INDEX;
-			if(config.IS_OVERFLOW && config.OVERFLOW_MODE==1) {
-				for (int beforeIndex = 0; beforeIndex < maxIndex; beforeIndex++) {
-					before.add(beforeIndex);
-				}
-			}
-			for (int m = 0; m < clientDevicesNum; m++) {
-				deviceCodes.add(config.DEVICE_CODES.get(index * clientDevicesNum + m));
-			}
-			while(i < config.LOOP) {
-				long oldTotalTime = totalTime.get();
-				if (config.BENCHMARK_WORK_MODE.equals(Constants.MODE_INSERT_TEST_WITH_USERDEFINED_PATH)) {
-					try {
-						database.insertGenDataOneBatch(config.STORAGE_GROUP_NAME + "." + config.TIMESERIES_NAME, i, totalTime, errorCount, latencies);
-					} catch (SQLException e) {
-						LOGGER.error("{} Fail to insert one batch into database becasue {}", Thread.currentThread().getName(), e.getMessage());
-					}
-				} else if (config.MUL_DEV_BATCH && !config.IS_OVERFLOW) {
-					try {
-						database.insertOneBatchMulDevice(deviceCodes, i, totalTime, errorCount, latencies);
-					} catch (SQLException e) {
-						LOGGER.error("{} Fail to insert one batch into database becasue {}", Thread.currentThread().getName(), e.getMessage());
-					}
-				} else if (!config.IS_OVERFLOW) {
-					try {
-						for (int m = 0; m < clientDevicesNum; m++) {
-							database.insertOneBatch(config.DEVICE_CODES.get(index * clientDevicesNum + m), i, totalTime, errorCount, latencies);
-						}
-					} catch (SQLException e) {
-						LOGGER.error("{} Fail to insert one batch into database becasue {}", Thread.currentThread().getName(), e.getMessage());
-					}
-				} else if (config.OVERFLOW_MODE == 1) {
-					try {
-						for (int m = 0; m < clientDevicesNum; m++) {
-							maxIndex = database.insertOverflowOneBatch(config.DEVICE_CODES.get(index * clientDevicesNum + m),
-									i,
-									totalTime,
-									errorCount,
-									before,
-									maxIndex,
-									random, latencies);
-						}
-					} catch (SQLException e) {
-						LOGGER.error("{} Fail to insert one batch into database becasue {}", Thread.currentThread().getName(), e.getMessage());
-					}
-				} else if (config.OVERFLOW_MODE == 2) {
-					try {
-						for (int m = 0; m < clientDevicesNum; m++) {
-							currMaxIndexOfDist = database.insertOverflowOneBatchDist(config.DEVICE_CODES.get(index * clientDevicesNum + m),
-									i,
-									totalTime,
-									errorCount,
-									currMaxIndexOfDist,
-									random, latencies);
-						}
-					} catch (SQLException e) {
-						LOGGER.error("{} Fail to insert one batch into database becasue {}", Thread.currentThread().getName(), e.getMessage());
-					}
-				} else {
-					System.out.println("unsupported overflow mode:" + config.OVERFLOW_MODE);
-					break;
-				}
-				i++;
+    if (config.READ_FROM_FILE) {
+      while (true) {
+        try {
+          LinkedList<String> cons = storage.consume(config.BATCH_OP_NUM);
+          //线程结束
+          if (cons.size() == 0) {
+            break;
+          }
+          database.insertOneBatch(cons, i, totalTime, errorCount, latencies);
+        } catch (SQLException e) {
+          LOGGER.error("{} Fail to insert one batch into database becasue {}",
+              Thread.currentThread().getName(), e.getMessage());
+        }
+        i++;
+      }
+    } else {
+      int clientDevicesNum = config.DEVICE_NUMBER / config.CLIENT_NUMBER;
+      LinkedList<String> deviceCodes = new LinkedList<>();
+      //may not correct in multiple device per batch mode
+      long pointsOneLoop =
+          config.DEVICE_NUMBER / config.CLIENT_NUMBER * config.SENSOR_NUMBER * config.BATCH_SIZE;
+      double actualLoopSecond = (double) pointsOneLoop / config.CLIENT_MAX_WRT_RATE;
+      //overflow mode 2 related variables initial
+      Random random = new Random(config.QUERY_SEED);
+      ArrayList<Integer> before = new ArrayList<>();
+      int maxIndex = (int) (config.BATCH_SIZE * config.LOOP * config.OVERFLOW_RATIO);
+      int currMaxIndexOfDist = config.START_TIMESTAMP_INDEX;
+      if (config.IS_OVERFLOW && config.OVERFLOW_MODE == 1) {
+        for (int beforeIndex = 0; beforeIndex < maxIndex; beforeIndex++) {
+          before.add(beforeIndex);
+        }
+      }
+      for (int m = 0; m < clientDevicesNum; m++) {
+        deviceCodes.add(config.DEVICE_CODES.get(index * clientDevicesNum + m));
+      }
+      while (i < config.LOOP) {
+        long oldTotalTime = totalTime.get();
+        if (config.BENCHMARK_WORK_MODE.equals(Constants.MODE_INSERT_TEST_WITH_USERDEFINED_PATH)) {
+          try {
+            database
+                .insertGenDataOneBatch(config.STORAGE_GROUP_NAME + "." + config.TIMESERIES_NAME, i,
+                    totalTime, errorCount, latencies);
+          } catch (SQLException e) {
+            LOGGER.error("{} Fail to insert one batch into database becasue {}",
+                Thread.currentThread().getName(), e.getMessage());
+          }
+        } else if (config.MUL_DEV_BATCH && !config.IS_OVERFLOW) {
+          try {
+            database.insertOneBatchMulDevice(deviceCodes, i, totalTime, errorCount, latencies);
+          } catch (SQLException e) {
+            LOGGER.error("{} Fail to insert one batch into database becasue {}",
+                Thread.currentThread().getName(), e.getMessage());
+          }
+        } else if (!config.IS_OVERFLOW) {
+          try {
+            for (int m = 0; m < clientDevicesNum; m++) {
+              LOGGER.info("进入数据顺序插入模式");
+              database.insertOneBatch(config.DEVICE_CODES.get(index * clientDevicesNum + m), i,
+                  totalTime, errorCount, latencies);
+            }
+          } catch (SQLException e) {
+            LOGGER.error("{} Fail to insert one batch into database becasue {}",
+                Thread.currentThread().getName(), e.getMessage());
+          }
+        } else if (config.OVERFLOW_MODE == 1) {
+          try {
+            for (int m = 0; m < clientDevicesNum; m++) {
+              maxIndex = database
+                  .insertOverflowOneBatch(config.DEVICE_CODES.get(index * clientDevicesNum + m),
+                      i,
+                      totalTime,
+                      errorCount,
+                      before,
+                      maxIndex,
+                      random, latencies);
+            }
+          } catch (SQLException e) {
+            LOGGER.error("{} Fail to insert one batch into database becasue {}",
+                Thread.currentThread().getName(), e.getMessage());
+          }
+        } else if (config.OVERFLOW_MODE == 2) {
+          try {
+            for (int m = 0; m < clientDevicesNum; m++) {
+              currMaxIndexOfDist = database
+                  .insertOverflowOneBatchDist(config.DEVICE_CODES.get(index * clientDevicesNum + m),
+                      i,
+                      totalTime,
+                      errorCount,
+                      currMaxIndexOfDist,
+                      random, latencies);
+            }
+          } catch (SQLException e) {
+            LOGGER.error("{} Fail to insert one batch into database becasue {}",
+                Thread.currentThread().getName(), e.getMessage());
+          }
+        } else {
+          System.out.println("unsupported overflow mode:" + config.OVERFLOW_MODE);
+          break;
+        }
+        i++;
 
-				long loopDeltaTime = totalTime.get() - oldTotalTime;
+        long loopDeltaTime = totalTime.get() - oldTotalTime;
 
-				double loopSecond = loopDeltaTime * 0.000000001d;
+        LOGGER.info("执行一个batch的总时间为{}", loopDeltaTime);
 
-				double loopRate = pointsOneLoop / loopSecond;
+        double loopSecond = loopDeltaTime * 0.000000001d;
 
-				if (config.USE_OPS) {
-					long delayStart = System.nanoTime();
-					if (loopSecond < actualLoopSecond) {
-						try {
-							Thread.sleep((long) (1000 * (actualLoopSecond - loopSecond)));
-						} catch (InterruptedException e) {
-							e.printStackTrace();
-						}
-					}
-					long delayEnd = System.nanoTime();
-					long delayTime = delayEnd - delayStart;
-					loopRate = pointsOneLoop / (loopSecond + delayTime * 0.000000001d);
-				}
+        double loopRate = pointsOneLoop / loopSecond;
 
-				LOGGER.info("{} LOOP ,{}, INSERT RATE,{},points/s,LOOP DELTA TIME,{},second", Thread.currentThread().getName(), i, loopRate, loopSecond);
-				mySql.saveInsertProcessOfLoop(i, loopRate);
-			}
-		}
+        if (config.USE_OPS) {
+          long delayStart = System.nanoTime();
+          if (loopSecond < actualLoopSecond) {
+            try {
+              Thread.sleep((long) (1000 * (actualLoopSecond - loopSecond)));
+            } catch (InterruptedException e) {
+              e.printStackTrace();
+            }
+          }
+          long delayEnd = System.nanoTime();
+          long delayTime = delayEnd - delayStart;
+          loopRate = pointsOneLoop / (loopSecond + delayTime * 0.000000001d);
+        }
 
+        LOGGER.info("{} LOOP ,{}, INSERT RATE,{},points/s,LOOP DELTA TIME,{},second",
+            Thread.currentThread().getName(), i, loopRate, loopSecond);
+        mySql.saveInsertProcessOfLoop(i, loopRate);
+      }
+    }
 
-		try {
-			database.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		this.totalTimes.add(totalTime.get());
-		this.totalInsertErrorNums.add(errorCount.get());
-		this.latenciesOfClients.add(latencies);
-		this.downLatch.countDown();
-	}
+    try {
+      database.close();
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+    this.totalTimes.add(totalTime.get());
+    this.totalInsertErrorNums.add(errorCount.get());
+    this.latenciesOfClients.add(latencies);
+    this.downLatch.countDown();
+    LOGGER.info("总耗时{},总延迟{}", totalTimes, latenciesOfClients.size());
+  }
 
 }
