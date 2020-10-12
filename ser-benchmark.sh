9eafcb283aa5fcb1b6f235713b26c4711d3ddaac
Boris <969032536@qq.com>
2020-10-12 14:35:36 +0800

    Update ser-benchmark.sh

--- ser-benchmark.sh
+++ ser-benchmark.sh
@@ -11,15 +11,11 @@ echo $BENCHMARK_HOME
 #sed -i 's/SERVER_MODE *= *false/SERVER_MODE=true/g' $BENCHMARK_HOME/conf/config.properties
 sed -i "s/^BENCHMARK_WORK_MODE.*$/BENCHMARK_WORK_MODE=serverMODE/g" $BENCHMARK_HOME/conf/config.properties
 
-cd $BENCHMARK_HOME
-
 echo '------Server Test Begin Time------'
 
 date
 
-cd bin
-
-sh startup.sh -cf ../conf/config.properties
+$BENCHMARK_HOME/bin/startup.sh -cf $BENCHMARK_HOME/conf/config.properties
 
 echo '------Server Test Complete Time------'
 
