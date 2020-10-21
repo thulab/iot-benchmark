aaf4b5737029ac3d9e12df75087fce74ab2a83dd
Boris <969032536@qq.com>
2020-10-21 15:24:09 +0800

    recorder use a statement

--- core/src/main/java/cn/edu/tsinghua/iotdb/benchmark/measurement/persistence/mysql/MySqlRecorder.java
+++ core/src/main/java/cn/edu/tsinghua/iotdb/benchmark/measurement/persistence/mysql/MySqlRecorder.java
@@ -65,13 +65,11 @@ public class MySqlRecorder implements ITestDataPersistence {
 
   // 检查记录本次实验的表格是否已经创建，没有则创建
   private void initTable() {
-    Statement stat = null;
     try {
-      stat = mysqlConnection.createStatement();
       if (config.getBENCHMARK_WORK_MODE().equals(Constants.MODE_SERVER_MODE)
           || config.getBENCHMARK_WORK_MODE().equals(Constants.MODE_CLIENT_SYSTEM_INFO)) {
         if (!hasTable("SERVER_MODE_" + localName + "_" + day)) {
-          stat.executeUpdate("create table SERVER_MODE_"
+          statement.executeUpdate("create table SERVER_MODE_"
               + localName
               + "_"
               + day
@@ -85,18 +83,18 @@ public class MySqlRecorder implements ITestDataPersistence {
         return;
       }
       if (!hasTable("CONFIG")) {
-        stat.executeUpdate(
+        statement.executeUpdate(
             "create table CONFIG (id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT, projectID VARCHAR(150), configuration_item VARCHAR(150), configuration_value VARCHAR(150))AUTO_INCREMENT = 1;");
         LOGGER.info("Table CONFIG create success!");
       }
       if (!hasTable("FINAL_RESULT")) {
-        stat.executeUpdate(
+        statement.executeUpdate(
             "create table FINAL_RESULT (id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT, projectID VARCHAR(150), operation VARCHAR(50), result_key VARCHAR(150), result_value VARCHAR(150))AUTO_INCREMENT = 1;");
         LOGGER.info("Table FINAL_RESULT create success!");
       }
       if (config.getBENCHMARK_WORK_MODE().equals(Constants.MODE_TEST_WITH_DEFAULT_PATH) && !hasTable(
           projectID)) {
-        stat.executeUpdate("create table "
+        statement.executeUpdate("create table "
             + projectID
             + "(id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT, recordTime varchar(50), clientName varchar(50), "
             + "operation varchar(50), okPoint INTEGER, failPoint INTEGER, latency DOUBLE, rate DOUBLE, remark varchar(1000))AUTO_INCREMENT = 1;");
@@ -104,23 +102,13 @@ public class MySqlRecorder implements ITestDataPersistence {
       }
     } catch (SQLException e) {
       LOGGER.error("mysql 创建表格失败,原因是", e);
-    } finally {
-      try {
-        if (stat != null) {
-          stat.close();
-        }
-      } catch (SQLException e) {
-        LOGGER.error("close statement failed", e);
-      }
     }
   }
 
   @Override
   public void insertSystemMetrics(Map<SystemMetrics, Float> systemMetricsMap) {
-    Statement stat = null;
     String sql = "";
     try {
-      stat = mysqlConnection.createStatement();
       sql = String.format("insert into SERVER_MODE_" + localName
               + "_" + day + " values(%d,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f)",
           System.currentTimeMillis(),
@@ -138,17 +126,9 @@ public class MySqlRecorder implements ITestDataPersistence {
           systemMetricsMap.get(SystemMetrics.DISK_TPS),
           systemMetricsMap.get(SystemMetrics.DISK_READ_SPEED_MB),
           systemMetricsMap.get(SystemMetrics.DISK_WRITE_SPEED_MB));
-      stat.executeUpdate(sql);
+      statement.executeUpdate(sql);
     } catch (SQLException e) {
       LOGGER.error("{} insert into mysql failed", sql, e);
-    } finally {
-      if (stat != null) {
-        try {
-          stat.close();
-        } catch (SQLException e) {
-          LOGGER.error("close statement failed", e);
-        }
-      }
     }
   }
 
@@ -167,7 +147,6 @@ public class MySqlRecorder implements ITestDataPersistence {
             time, Thread.currentThread().getName(), operation, okPoint, failPoint, latency, rate,
             remark);
     try {
-      statement = mysqlConnection.createStatement();
       statement.execute(mysqlSql);
       count ++;
       if(count % 5000 == 0) {
@@ -193,53 +172,35 @@ public class MySqlRecorder implements ITestDataPersistence {
           "{} save saveInsertProcess info into mysql failed! Error：{}",
           Thread.currentThread().getName(), e.getMessage());
       LOGGER.error("{}", mysqlSql);
-    } finally {
-      try {
-        statement.close();
-      } catch (SQLException throwable) {
-        LOGGER.error("Failed to close statement for config info because", throwable);
-      }
     }
   }
 
   // 存储实验结果
   @Override
   public void saveResult(String operation, String k, String v) {
-    Statement stat = null;
     String sql = String.format(SAVE_RESULT, projectID, operation, k, v);
     try {
-      stat = mysqlConnection.createStatement();
-      stat.executeUpdate(sql);
+      statement.executeUpdate(sql);
     } catch (SQLException e) {
       LOGGER.error("{}将结果信息写入mysql失败，because ：{}", sql, e);
-    } finally {
-      if (stat != null) {
-        try {
-          stat.close();
-        } catch (SQLException e) {
-          LOGGER.error("Failed to close statement for saving result because", e);
-        }
-      }
     }
   }
 
   @Override
   public void saveTestConfig() {
-    Statement stat = null;
     String sql = "";
     try {
-      stat = mysqlConnection.createStatement();
       if (config.getBENCHMARK_WORK_MODE().equals(Constants.MODE_TEST_WITH_DEFAULT_PATH)) {
         sql = String.format(SAVE_CONFIG, "'" + projectID + "'",
             "'MODE'", "'DEFAULT_TEST_MODE'");
-        stat.addBatch(sql);
+        statement.addBatch(sql);
       }
       switch (config.getDB_SWITCH().trim()) {
         case Constants.DB_IOT:
         case Constants.DB_TIMESCALE:
           sql = String.format(SAVE_CONFIG, "'" + projectID + "'",
               "'ServerIP'", "'" + config.getHOST() + "'");
-          stat.addBatch(sql);
+          statement.addBatch(sql);
           break;
         case Constants.DB_INFLUX:
         case Constants.DB_OPENTS:
@@ -249,101 +210,93 @@ public class MySqlRecorder implements ITestDataPersistence {
               .substring(config.getDB_URL().lastIndexOf('/') + 1, config.getDB_URL().lastIndexOf(':'));
           sql = String.format(SAVE_CONFIG, "'" + projectID + "'",
               "'ServerIP'", "'" + host + "'");
-          stat.addBatch(sql);
+          statement.addBatch(sql);
           break;
         default:
           throw new SQLException("unsupported database " + config.getDB_SWITCH());
       }
       sql = String.format(SAVE_CONFIG, "'" + projectID + "'",
           "'CLIENT'", "'" + localName + "'");
-      stat.addBatch(sql);
+      statement.addBatch(sql);
       sql = String.format(SAVE_CONFIG, "'" + projectID + "'",
           "'DB_SWITCH'", "'" + config.getDB_SWITCH() + "'");
-      stat.addBatch(sql);
+      statement.addBatch(sql);
       sql = String.format(SAVE_CONFIG, "'" + projectID + "'",
           "'VERSION'", "'" + config.getVERSION() + "'");
-      stat.addBatch(sql);
+      statement.addBatch(sql);
       sql = String.format(SAVE_CONFIG, "'" + projectID + "'",
           "'getCLIENT_NUMBER()'", "'" + config.getCLIENT_NUMBER() + "'");
-      stat.addBatch(sql);
+      statement.addBatch(sql);
       sql = String.format(SAVE_CONFIG, "'" + projectID + "'", "'LOOP'",
           "'" + config.getLOOP() + "'");
-      stat.addBatch(sql);
+      statement.addBatch(sql);
       if (config.getBENCHMARK_WORK_MODE()
           .equals(Constants.MODE_TEST_WITH_DEFAULT_PATH)) {
         sql = String.format(SAVE_CONFIG, "'" + projectID + "'",
             "'查询数据集存储组数'",
             "'" + config.getGROUP_NUMBER() + "'");
-        stat.addBatch(sql);
+        statement.addBatch(sql);
         sql = String.format(SAVE_CONFIG, "'" + projectID + "'",
             "'查询数据集设备数'", "'" + config.getDEVICE_NUMBER()
                 + "'");
-        stat.addBatch(sql);
+        statement.addBatch(sql);
         sql = String.format(SAVE_CONFIG, "'" + projectID + "'",
             "'查询数据集传感器数'", "'" + config.getSENSOR_NUMBER()
                 + "'");
-        stat.addBatch(sql);
+        statement.addBatch(sql);
         if (config.getDB_SWITCH().equals(Constants.DB_IOT)) {
           sql = String.format(SAVE_CONFIG, "'" + projectID + "'",
               "'IOTDB编码方式'", "'" + config.getENCODING() + "'");
-          stat.addBatch(sql);
+          statement.addBatch(sql);
         }
 
         sql = String.format(SAVE_CONFIG, "'" + projectID + "'",
             "'QUERY_DEVICE_NUM'", "'" + config.getQUERY_DEVICE_NUM()
                 + "'");
-        stat.addBatch(sql);
+        statement.addBatch(sql);
         sql = String.format(SAVE_CONFIG, "'" + projectID + "'",
             "'QUERY_SENSOR_NUM'", "'" + config.getQUERY_SENSOR_NUM()
                 + "'");
-        stat.addBatch(sql);
+        statement.addBatch(sql);
 
         sql = String.format(SAVE_CONFIG, "'" + projectID + "'",
             "'IS_OVERFLOW'", "'" + config.isIS_OVERFLOW() + "'");
-        stat.addBatch(sql);
+        statement.addBatch(sql);
         if (config.isIS_OVERFLOW()) {
           sql = String.format(SAVE_CONFIG, "'" + projectID + "'",
               "'OVERFLOW_RATIO'", "'" + config.getOVERFLOW_RATIO() + "'");
-          stat.addBatch(sql);
+          statement.addBatch(sql);
         }
         sql = String.format(SAVE_CONFIG, "'" + projectID + "'",
             "'MUL_DEV_BATCH'", "'" + config.isMUL_DEV_BATCH() + "'");
-        stat.addBatch(sql);
+        statement.addBatch(sql);
         sql = String.format(SAVE_CONFIG, "'" + projectID + "'",
             "'DEVICE_NUMBER'", "'" + config.getDEVICE_NUMBER() + "'");
-        stat.addBatch(sql);
+        statement.addBatch(sql);
         sql = String.format(SAVE_CONFIG, "'" + projectID + "'",
             "'GROUP_NUMBER'", "'" + config.getGROUP_NUMBER() + "'");
-        stat.addBatch(sql);
+        statement.addBatch(sql);
         sql = String.format(SAVE_CONFIG, "'" + projectID + "'",
             "'DEVICE_NUMBER'", "'" + config.getDEVICE_NUMBER() + "'");
-        stat.addBatch(sql);
+        statement.addBatch(sql);
         sql = String.format(SAVE_CONFIG, "'" + projectID + "'",
             "'SENSOR_NUMBER'", "'" + config.getSENSOR_NUMBER() + "'");
-        stat.addBatch(sql);
+        statement.addBatch(sql);
         sql = String.format(SAVE_CONFIG, "'" + projectID + "'",
             "'BATCH_SIZE'", "'" + config.getBATCH_SIZE() + "'");
-        stat.addBatch(sql);
+        statement.addBatch(sql);
         sql = String.format(SAVE_CONFIG, "'" + projectID + "'",
             "'POINT_STEP'", "'" + config.getPOINT_STEP() + "'");
-        stat.addBatch(sql);
+        statement.addBatch(sql);
         if (config.getDB_SWITCH().equals(Constants.DB_IOT)) {
           sql = String.format(SAVE_CONFIG, "'" + projectID + "'",
               "'ENCODING'", "'" + config.getENCODING() + "'");
-          stat.addBatch(sql);
+          statement.addBatch(sql);
         }
       }
-      stat.executeBatch();
+      statement.executeBatch();
     } catch (SQLException e) {
       LOGGER.error("{}将配置信息写入mysql失败，because ：{}", sql, e);
-    } finally {
-      if (stat != null) {
-        try {
-          stat.close();
-        } catch (SQLException e) {
-          LOGGER.error("Failed to close statement for config info because", e);
-        }
-      }
     }
   }
 
