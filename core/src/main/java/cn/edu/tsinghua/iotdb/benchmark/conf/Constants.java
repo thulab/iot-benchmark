6d03d51f0850ea523b662719910f14830b8b20ab
xiangdong huang <sainthxd@gmail.com>
2021-03-19 12:05:56 +0800

    fix cluster session error

--- core/src/main/java/cn/edu/tsinghua/iotdb/benchmark/conf/Constants.java
+++ core/src/main/java/cn/edu/tsinghua/iotdb/benchmark/conf/Constants.java
@@ -61,7 +61,7 @@ public class Constants {
     public static final String IOTDB012_JDBC_CLASS = "cn.edu.tsinghua.iotdb.benchmark.iotdb012.IoTDB";
     public static final String IOTDB012_DOUBLE_JDBC_CLASS = "cn.edu.tsinghua.iotdb.benchmark.iotdb012.DoubleIoTDBChecker";
     public static final String IOTDB012_SESSION_CLASS = "cn.edu.tsinghua.iotdb.benchmark.iotdb012.IoTDBSession";
-    public static final String IOTDB012_ROUNDROBIN_SESSION_CLASS = "cn.edu.tsinghua.iotdb.benchmark.iotdb012.IoTDBSession";
+    public static final String IOTDB012_ROUNDROBIN_SESSION_CLASS = "cn.edu.tsinghua.iotdb.benchmark.iotdb012.IoTDBClusterSession";
 
     public static final String IOTDB011_JDBC_CLASS = "cn.edu.tsinghua.iotdb.benchmark.iotdb011.IoTDB";
     public static final String IOTDB011_DOUBLE_JDBC_CLASS = "cn.edu.tsinghua.iotdb.benchmark.iotdb011.DoubleIoTDBChecker";
