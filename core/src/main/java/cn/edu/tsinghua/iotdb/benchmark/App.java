8ca566bd6b47eff717c0f1a40762530395c586d5
stormbroken <181250184@smail.nju.edu.cn>
2021-08-13 12:59:43 +0800

    remove QueryWithRealDataSetMode.java

--- core/src/main/java/cn/edu/tsinghua/iotdb/benchmark/App.java
+++ core/src/main/java/cn/edu/tsinghua/iotdb/benchmark/App.java
@@ -50,9 +50,6 @@ public class App {
       case Constants.MODE_TEST_WITH_DEFAULT_PATH:
         baseMode = new TestWithDefaultPathMode();
         break;
-      case Constants.MODE_QUERY_WITH_REAL_DATASET:
-        baseMode = new QueryWithRealDataSetMode();
-        break;
       case Constants.MODE_SERVER_MODE:
         baseMode = new ServerMode();
         break;
