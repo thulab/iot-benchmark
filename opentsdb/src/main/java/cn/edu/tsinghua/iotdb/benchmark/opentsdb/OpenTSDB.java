4233f48bbffd02fd484be41b4466602d388e3609
Xuan Ronaldo <xuanronaldo@qq.com>
2021-10-11 11:54:58 +0800

    Fix pr 204 (#206)
    * Fix the issue of #201.
    
    * Modify the README.md
    
    * add the version number of the opentsdb.
    
    * add the version number of the opentsdb.

--- opentsdb/src/main/java/cn/edu/tsinghua/iotdb/benchmark/opentsdb/OpenTSDB.java
+++ opentsdb/src/main/java/cn/edu/tsinghua/iotdb/benchmark/opentsdb/OpenTSDB.java
@@ -161,6 +161,7 @@ public class OpenTSDB implements IDatabase {
     queryMap.put("start", aggRangeQuery.getStartTimestamp() - 1);
     queryMap.put("end", aggRangeQuery.getEndTimestamp() + 1);
     list = getSubQueries(aggRangeQuery.getDeviceSchema(), aggRangeQuery.getAggFun());
+    list.get(0).put("downsample", "0all-count");
     queryMap.put("queries", list);
     String sql = JSON.toJSONString(queryMap);
     return executeQueryAndGetStatus(sql, false);
