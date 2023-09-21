24817a347f1b18497bc5a7b8c263e7545eb8a546
liyuheng <liyuheng55555@126.com>
2023-09-21 12:16:43 +0800

    insertByTablet execute synchronize

--- iotdb-1.1/src/main/java/cn/edu/tsinghua/iot/benchmark/iotdb110/IoTDBSessionBase.java
+++ iotdb-1.1/src/main/java/cn/edu/tsinghua/iot/benchmark/iotdb110/IoTDBSessionBase.java
@@ -71,20 +71,16 @@ public class IoTDBSessionBase extends IoTDB {
 
   public Status insertOneBatchByTablet(IBatch batch) {
     Tablet tablet = genTablet(batch);
-    future =
-        service.submit(
-            () -> {
-              try {
-                if (config.isVECTOR()) {
-                  sessionWrapper.insertAlignedTablet(tablet);
-                } else {
-                  sessionWrapper.insertTablet(tablet);
-                }
-              } catch (IoTDBConnectionException | StatementExecutionException e) {
-                LOGGER.error("insert tablet failed", e);
-              }
-            });
-    return waitFuture();
+    try {
+      if (config.isVECTOR()) {
+        sessionWrapper.insertAlignedTablet(tablet);
+      } else {
+        sessionWrapper.insertTablet(tablet);
+      }
+      return new Status(true);
+    } catch (IoTDBConnectionException | StatementExecutionException e) {
+      return new Status(false, 0, e, e.toString());
+    }
   }
 
   public Status insertOneBatchByRecord(IBatch batch) {
