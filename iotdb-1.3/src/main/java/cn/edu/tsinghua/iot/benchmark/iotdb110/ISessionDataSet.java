748a57604a0de6fc64ccff97e4ad76e019a8e9c7
JackieTien97 <jackietien97@gmail.com>
2023-12-12 16:25:26 +0800

    add iotdb-1.3

---
+++ iotdb-1.3/src/main/java/cn/edu/tsinghua/iot/benchmark/iotdb110/ISessionDataSet.java
@@ -0,0 +1,16 @@
+package cn.edu.tsinghua.iot.benchmark.iotdb110;
+
+import org.apache.iotdb.isession.SessionDataSet;
+import org.apache.iotdb.rpc.IoTDBConnectionException;
+import org.apache.iotdb.rpc.StatementExecutionException;
+import org.apache.iotdb.tsfile.read.common.RowRecord;
+
+public interface ISessionDataSet {
+  RowRecord next() throws IoTDBConnectionException, StatementExecutionException;
+
+  boolean hasNext() throws IoTDBConnectionException, StatementExecutionException;
+
+  void close() throws IoTDBConnectionException, StatementExecutionException;
+
+  SessionDataSet.DataIterator iterator();
+}
